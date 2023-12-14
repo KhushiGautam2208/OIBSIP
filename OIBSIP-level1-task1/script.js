5b434ef76e40526cbc64338a1fe31106eecdd0f9
Khushi Gautam <148616836+KhushiGautam2208@users.noreply.github.com>
2023-12-14 23:09:47 +0530

    Add files via upload

---
+++ OIBSIP-level1-task1/script.js
@@ -0,0 +1,26 @@
+// alert("hello")
+
+const slides=document.querySelectorAll(".slider")
+var counter=0;
+
+slides.forEach(
+    (slides,index) =>{
+        slides.style.left=`${index*100}%`
+    }
+)
+
+const goPrev=()=>{
+    counter--
+    slideImage()
+}
+const goNext=()=>{
+    counter++
+    slideImage()
+}
+const slideImage=()=>{
+    slides.forEach(
+        (slide) =>{
+            slide.style.transform=`translateX(-$(counter*100)%)`
+        }
+    )
+}
